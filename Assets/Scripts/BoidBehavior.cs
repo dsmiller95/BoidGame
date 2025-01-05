45fe0e18ea09ad5b20f2a044b289a91ed503888d
Dan M <dsmiller95@gmail.com>
2025-01-05 10:32:08 -0600

    slightly better al gore rythms

--- Assets/Scripts/BoidBehavior.cs
+++ Assets/Scripts/BoidBehavior.cs
@@ -36,8 +36,9 @@ public class BoidBehavior : MonoBehaviour
     
     public void Initialize()
     {
-        var randDir = new Vector2(Mathf.Sin(Time.time), Mathf.Cos(Time.time));
-        var targetHeading = Vector2.Lerp(Vector2.right, randDir, config.randomMagnitude);
+        var cycleDir = new Vector2(Mathf.Sin(Time.time), Mathf.Cos(Time.time));
+        var randDir = UnityEngine.Random.insideUnitCircle.normalized;
+        var targetHeading = Vector2.Lerp(cycleDir, randDir, config.randomMagnitude);
         _rigidbody2D.linearVelocity = targetHeading * config.initialSpeed; 
         _spawnedAtTime = Time.fixedTime;
     }
@@ -88,15 +89,19 @@ public class BoidBehavior : MonoBehaviour
             if (otherBoid == null) continue;
 
             var neighborPosition = otherBoid._rigidbody2D.position;
-            Vector2 toNeighbor = otherBoid._rigidbody2D.position - _rigidbody2D.position;
+            var myPosition = _rigidbody2D.position;
+            Vector2 toNeighbor = neighborPosition - myPosition;
             float distance = toNeighbor.magnitude;
-            var toNeighborNormalized = toNeighbor / distance;
 
             // Separation
             if (distance < config.separationRadius)
             {
                 separationNeighborCount++;
-                separation += neighborPosition;//-toNeighborNormalized / distance;
+                var fromNeighbor = -toNeighbor;
+                var separationAdjustment = 1f - (distance / config.separationRadius);
+                //separationAdjustment = separationAdjustment * separationAdjustment;
+                //separationAdjustment = Mathf.Clamp01(separationAdjustment);
+                separation += fromNeighbor.normalized * (0.5f + separationAdjustment * 0.5f);
             }
 
             // Alignment and Cohesion
@@ -118,8 +123,8 @@ public class BoidBehavior : MonoBehaviour
         var currentPosition = _rigidbody2D.position;
         if(separationNeighborCount > 0)
         {
-            separation =  currentPosition - (separation / separationNeighborCount);
-            separation = separation.normalized;
+            //separation =  (currentPosition * separationNeighborCount) - separation;
+            //separation = separation.normalized;
             //if(separation.magnitude > config.maxForce) separation = separation.normalized * config.maxForce;
             DrawSteeringForce(separation, Color.blue);
         }
@@ -127,8 +132,8 @@ public class BoidBehavior : MonoBehaviour
         
         if(alignmentNeighborCount > 0)
         {
-            alignment = (alignment / alignmentNeighborCount) - forward;
-            alignment = alignment.normalized;
+            alignment = (alignment / alignmentNeighborCount) - _rigidbody2D.linearVelocity;
+            //alignment = alignment.normalized;
             //if(alignment.magnitude > config.maxForce) alignment = alignment.normalized * config.maxForce;
             DrawSteeringForce(alignment, Color.green);
         }
@@ -138,7 +143,7 @@ public class BoidBehavior : MonoBehaviour
         {
             cohesion /= cohesionNeighborCount;
             cohesion -= _rigidbody2D.position;
-            cohesion = cohesion.normalized;
+            //cohesion = cohesion.normalized;
             //if(cohesion.magnitude > config.maxForce) cohesion = cohesion.normalized * config.maxForce;
             DrawSteeringForce(cohesion, Color.red);
         }
@@ -148,13 +153,14 @@ public class BoidBehavior : MonoBehaviour
         Debug.DrawLine(_rigidbody2D.position, steerFrom, Color.magenta);
 
         var targetForward = separation * config.separationWeight + alignment * config.alignmentWeight + cohesion * config.cohesionWeight;
-        var nextHeading = (forward + Time.fixedDeltaTime * (targetForward - forward)).normalized;
-        
-        //_currentSteering = Vector2.Lerp(_currentSteering, steerForce, 0.1f);
+        var nextHeading = _rigidbody2D.linearVelocity + Time.fixedDeltaTime * (targetForward - _rigidbody2D.linearVelocity);
+        nextHeading = ClampMagnitude(nextHeading, config.minSpeed, config.maxSpeed);
+        //nextHeading = nextHeading.normalized;
 
-        _rigidbody2D.linearVelocity = nextHeading * config.maxSpeed;
+        _rigidbody2D.linearVelocity = nextHeading; //* config.maxSpeed;
         _rigidbody2D.rotation = Mathf.Atan2(nextHeading.y, nextHeading.x) * Mathf.Rad2Deg;
         
+        //_currentSteering = Vector2.Lerp(_currentSteering, steerForce, 0.1f);
         // Vector2 velocity = _rigidbody2D.linearVelocity;
         // velocity += _currentSteering;
         // velocity = Vector2.ClampMagnitude(velocity, config.maxSpeed);
@@ -166,13 +172,34 @@ public class BoidBehavior : MonoBehaviour
         //_rigidbody2D.linearVelocity = velocity;
     }
 
+    private Vector2 ClampMagnitude(Vector2 heading, float min, float max)
+    {
+        var mag = heading.magnitude;
+        if (mag < min) return heading.normalized * min;
+        if (mag > max) return heading.normalized * max;
+        return heading;
+    }
+
     private float _velocityTimeAheadDraw = 0.5f;
     private void DrawSteeringForce(Vector2 steering, Color color)
     {
         var velocity = _rigidbody2D.linearVelocity;
         var position = _rigidbody2D.position;
-        var steerFrom = position + velocity * _velocityTimeAheadDraw;
+        var steerFrom = position;// + velocity * _velocityTimeAheadDraw;
         
         Debug.DrawLine(steerFrom, steerFrom + steering, color);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // draw circles for each radius in their color
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, config.separationRadius);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, config.alignmentRadius);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, config.cohesionRadius);
     }
 }
