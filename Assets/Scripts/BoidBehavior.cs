aeabd7a75a5c347bb5e4f9590949a6d33cac7b8c
Dan M <dsmiller95@gmail.com>
2025-01-05 09:29:14 -0600

    IT WORK

--- Assets/Scripts/BoidBehavior.cs
+++ Assets/Scripts/BoidBehavior.cs
@@ -28,11 +28,17 @@ public class BoidBehavior : MonoBehaviour
         public float cohesionWeight = 1f;
         
         public float lifetimeSeconds = 10f;
+        
+        [Range(0, 1)]
+        public float randomMagnitude = 0.1f;
+
     }
     
     public void Initialize()
     {
-        _rigidbody2D.linearVelocity = Vector2.right * config.initialSpeed;// new Vector2(Mathf.Sin(Time.time), Mathf.Cos(Time.time)) * config.initialSpeed;
+        var randDir = new Vector2(Mathf.Sin(Time.time), Mathf.Cos(Time.time));
+        var targetHeading = Vector2.Lerp(Vector2.right, randDir, config.randomMagnitude);
+        _rigidbody2D.linearVelocity = targetHeading * config.initialSpeed; 
         _spawnedAtTime = Time.fixedTime;
     }
     
@@ -60,7 +66,9 @@ public class BoidBehavior : MonoBehaviour
         Vector2 separation = Vector2.zero;
         Vector2 alignment = Vector2.zero;
         Vector2 cohesion = Vector2.zero;
-        int neighborCount = 0;
+        int separationNeighborCount = 0;
+        int alignmentNeighborCount = 0;
+        int cohesionNeighborCount = 0;
 
         var myLayer = _rigidbody2D.gameObject.layer;
         var contactFilter = new ContactFilter2D()
@@ -78,8 +86,6 @@ public class BoidBehavior : MonoBehaviour
             if (neighbor == _collider2D) continue;
             BoidBehavior otherBoid = neighbor.GetComponent<BoidBehavior>();
             if (otherBoid == null) continue;
-            
-            neighborCount++;
 
             var neighborPosition = otherBoid._rigidbody2D.position;
             Vector2 toNeighbor = otherBoid._rigidbody2D.position - _rigidbody2D.position;
@@ -89,46 +95,57 @@ public class BoidBehavior : MonoBehaviour
             // Separation
             if (distance < config.separationRadius)
             {
+                separationNeighborCount++;
                 separation += neighborPosition;//-toNeighborNormalized / distance;
             }
 
             // Alignment and Cohesion
             if (distance < config.alignmentRadius)
             {
+                alignmentNeighborCount++;
                 alignment += otherBoid._rigidbody2D.linearVelocity;
             }
 
             if (distance < config.cohesionRadius)
             {
+                cohesionNeighborCount++;
                 cohesion += neighborPosition;
             }
         }
 
         
         var forward = _rigidbody2D.linearVelocity.normalized;
-        if (neighborCount > 0)
+        var currentPosition = _rigidbody2D.position;
+        if(separationNeighborCount > 0)
         {
-            var currentPosition = _rigidbody2D.position;
-            
-            separation =  currentPosition - (separation / neighborCount);
+            separation =  currentPosition - (separation / separationNeighborCount);
             separation = separation.normalized;
             //if(separation.magnitude > config.maxForce) separation = separation.normalized * config.maxForce;
             DrawSteeringForce(separation, Color.blue);
-            
-            alignment = (alignment / neighborCount) - forward;
+        }
+        else separation = Vector2.zero;
+        
+        if(alignmentNeighborCount > 0)
+        {
+            alignment = (alignment / alignmentNeighborCount) - forward;
             alignment = alignment.normalized;
             //if(alignment.magnitude > config.maxForce) alignment = alignment.normalized * config.maxForce;
             DrawSteeringForce(alignment, Color.green);
+        }
+        else alignment = Vector2.zero;
 
-            cohesion /= neighborCount;
+        if (cohesionNeighborCount > 0)
+        {
+            cohesion /= cohesionNeighborCount;
             cohesion -= _rigidbody2D.position;
             cohesion = cohesion.normalized;
             //if(cohesion.magnitude > config.maxForce) cohesion = cohesion.normalized * config.maxForce;
             DrawSteeringForce(cohesion, Color.red);
-            
-            var steerFrom = _rigidbody2D.position + _rigidbody2D.linearVelocity * _velocityTimeAheadDraw;
-            Debug.DrawLine(_rigidbody2D.position, steerFrom, Color.magenta);
         }
+        else cohesion = Vector2.zero;
+        
+        var steerFrom = _rigidbody2D.position + _rigidbody2D.linearVelocity * _velocityTimeAheadDraw;
+        Debug.DrawLine(_rigidbody2D.position, steerFrom, Color.magenta);
 
         var targetForward = separation * config.separationWeight + alignment * config.alignmentWeight + cohesion * config.cohesionWeight;
         var nextHeading = (forward + Time.fixedDeltaTime * (targetForward - forward)).normalized;
@@ -136,6 +153,7 @@ public class BoidBehavior : MonoBehaviour
         //_currentSteering = Vector2.Lerp(_currentSteering, steerForce, 0.1f);
 
         _rigidbody2D.linearVelocity = nextHeading * config.maxSpeed;
+        _rigidbody2D.rotation = Mathf.Atan2(nextHeading.y, nextHeading.x) * Mathf.Rad2Deg;
         
         // Vector2 velocity = _rigidbody2D.linearVelocity;
         // velocity += _currentSteering;
