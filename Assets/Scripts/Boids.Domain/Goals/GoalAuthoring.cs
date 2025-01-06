2a3a4c8b5828edb64e224e39046136bfa37d3042
Dan M <dsmiller95@gmail.com>
2025-01-06 17:24:22 -0600

    add a goal that kinda renders

---
+++ Assets/Scripts/Boids.Domain/Goals/GoalAuthoring.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Boids.Domain.Goals
+{
+    [Serializable]
+    public struct Goal : IComponentData
+    {
+        public float radius;
+        public int required;
+    }
+    
+    [Serializable]
+    public struct GoalRender : IComponentData
+    {
+        public Entity scaleForProgress;
+    }
+
+    [Serializable]
+    public struct GoalCount : IComponentData
+    {
+        public int count;
+    }
+    
+    public class GoalAuthoring : MonoBehaviour
+    {
+        [Range(1f, 30f)]
+        public float radius = 1f;
+        public int required = 100;
+        public GameObject scaleForProgress = null!;
+
+        private void Awake()
+        {
+            if(scaleForProgress == null) throw new Exception("countText is null");
+        }
+
+        private class GoalBaker : Baker<GoalAuthoring>
+        {
+            public override void Bake(GoalAuthoring authoring)
+            {
+                DependsOn(authoring.scaleForProgress);
+
+                if (authoring.scaleForProgress == null) return;
+                    
+                var entity = GetEntity(TransformUsageFlags.Renderable);
+                AddComponent(entity, new Goal
+                {
+                    radius = authoring.radius,
+                    required = authoring.required,
+                });
+                AddComponent(entity, new GoalCount());
+                var scaleChildEntity = GetEntity(authoring.scaleForProgress, TransformUsageFlags.Dynamic);
+                if(scaleChildEntity == Entity.Null)
+                {
+                    throw new Exception("scaleForProgress entity is null");
+                }
+                var render = new GoalRender()
+                {
+                    scaleForProgress = scaleChildEntity,
+                };
+                AddComponent(entity, render);
+                
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            var scaledRadius = this.radius * this.transform.lossyScale.x;
+            Gizmos.DrawWireSphere(transform.position, scaledRadius);
+        }
+    }
+}
---
+++ Assets/Scripts/Boids.Domain/Goals/GoalRenderSystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Boids.Domain.Goals
+{
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
+    [BurstCompile]
+    public partial struct GoalRenderSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (goal, goalCount, render) in 
+                     SystemAPI.Query<RefRO<Goal>, RefRO<GoalCount>, RefRO<GoalRender>>())
+            {
+                var required = goal.ValueRO.required;
+                var count = goalCount.ValueRO.count;
+
+                var scale = math.min(1, count / (float)required);
+                var newLocalTransform = LocalTransform.FromScale(scale);
+                ecb.SetComponent(render.ValueRO.scaleForProgress, newLocalTransform);
+            }
+            ecb.Playback(state.EntityManager);
+        }
+    }
+}
