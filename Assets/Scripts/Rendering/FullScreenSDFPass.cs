ee9c7a85f78795961114dcc6e64ac0506e5bc33e
Dan M <dsmiller95@gmail.com>
2025-01-09 09:33:29 -0600

    try my hardest to do a render pass

---
+++ Assets/Scripts/Rendering/FullScreenSDFPass.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
+using UnityEngine.Rendering.Universal;
+
+namespace Rendering
+{
+    public class FullScreenSDFPass : ScriptableRenderPass
+    {
+        private string _profilerTag;
+        private Material _sdfMaterial;
+        
+        private static readonly int horizontalBlurId = Shader.PropertyToID("_HorizontalBlur");
+        private static readonly int verticalBlurId = Shader.PropertyToID("_VerticalBlur");
+        private const string k_BlurTextureName = "_BlurTexture";
+        private const string k_SdfPassName = "SdfRenderPass";
+        private const string k_HorizontalPassName = "HorizontalBlurRenderPass";
+    
+        // Optionally, track the pass event, e.g. "AfterRendering"
+        public FullScreenSDFPass(
+            string profilerTag,
+            RenderPassEvent renderPassEvent,
+            Material sdfMaterial)
+        {
+            this._profilerTag = profilerTag;
+            this.renderPassEvent = renderPassEvent;
+            this._sdfMaterial = sdfMaterial;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            base.Execute(context, ref renderingData);
+        }
+        
+        
+
+        public override void RecordRenderGraph(
+            RenderGraph renderGraph,
+            ContextContainer frameData)
+        {
+            // using var builder = renderGraph.AddRast1
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Rendering/FullScreenSDFRendererFeature.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Rendering
+{
+    public class FullScreenSDFRendererFeature : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class SDFSettings
+        {
+            public Material sdfMaterial = null;
+            public RenderPassEvent passEvent = RenderPassEvent.AfterRendering;
+        }
+        
+        [SerializeField] private SDFSettings settings;
+    
+        private FullScreenSDFPass _sdfPass;
+
+        public override void Create()
+        {
+            if(settings.sdfMaterial != null)
+            {
+                _sdfPass = new FullScreenSDFPass(
+                    "FullScreenSDFPass",
+                    settings.passEvent,
+                    settings.sdfMaterial
+                );
+            }
+        }
+
+        public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+        {
+            if (_sdfPass == null || settings.sdfMaterial == null) return;
+            
+            if (renderingData.cameraData.cameraType is CameraType.Game or CameraType.SceneView)
+            {
+                _sdfPass.ConfigureInput(ScriptableRenderPassInput.Color);
+                //_sdfPass.SetTarget(renderer.cameraColorTargetHandle, m_Intensity);
+            }
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (_sdfPass == null || settings.sdfMaterial == null) return;
+            
+            if (renderingData.cameraData.cameraType is CameraType.Game or CameraType.SceneView)
+            {
+                renderer.EnqueuePass(_sdfPass);
+            }
+        }
+        
+    }
+}
