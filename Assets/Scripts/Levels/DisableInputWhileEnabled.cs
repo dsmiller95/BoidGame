8164c8243d3741f3d8ad73673c721ddb1d99cab8
Dan M <dsmiller95@gmail.com>
2025-01-10 16:26:17 -0600

    disable game input when game end screen reached

---
+++ Assets/Scripts/Levels/DisableInputWhileEnabled.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Boids.Domain.OnClick;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Levels
+{
+    public class DisableInputWhileEnabled : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            var world = World.DefaultGameObjectInjectionWorld;
+            var inputSystem = world.GetExistingSystemManaged<InputSystemGroup>();
+            inputSystem.Enabled = false;
+        }
+
+        private void OnDisable()
+        {
+            var world = World.DefaultGameObjectInjectionWorld;
+            var inputSystem = world.GetExistingSystemManaged<InputSystemGroup>();
+            inputSystem.Enabled = true;
+        }
+    }
+}
