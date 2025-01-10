c39cbf62e8c507bcfc7556ef3c3554d8b12a550d
Dan M <dsmiller95@gmail.com>
2025-01-10 17:39:27 -0600

    add a timescale slider

---
+++ Assets/Scripts/Other/SetTimeScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Other
+{
+    public class SetTimeScale : MonoBehaviour
+    {
+        public float stepMultiple = 0.1f;
+
+        public void SetScale(float timeScale)
+        {
+            Time.timeScale = timeScale * stepMultiple;
+        }
+
+        private void OnEnable()
+        {
+            Time.timeScale = 1;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
