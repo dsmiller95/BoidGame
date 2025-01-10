18da7f31b82c2e0cf2fd87aea774b55a3365c994
Dan M <dsmiller95@gmail.com>
2025-01-10 16:54:42 -0600

    use a quad instead of a sprite fo the boids

---
+++ Assets/Scripts/Boids.Domain/BoidColors/ColorFromSpeedSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+
+namespace Boids.Domain.BoidColors
+{
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
+    public partial struct ColorFromSpeedSystem : ISystem
+    {
+        // public void OnUpdate(ref SystemState state)
+        // {
+        //     var query = SystemAPI.QueryBuilder()
+        //         .WithAll<SpriteRenderer, DebugFlagComponent>()
+        //         .Build();
+        //     var job = new WriteDebugColors();
+        //     job.Run(query);
+        // }
+        //
+        //
+        // private partial struct WriteColorFromVelocity : IJobEntity
+        // {
+        //     private void Execute(in DebugFlagComponent flag, SpriteRenderer spriteRenderer)
+        //     {
+        //         var color = flag.flag switch
+        //         {
+        //             FlagType.None => Color.white,
+        //             FlagType.Secondary => Color.red,
+        //             FlagType.Primary => Color.magenta,
+        //             _ => throw new ArgumentOutOfRangeException()
+        //         };
+        //         spriteRenderer.color = color;
+        //     }
+        // }
+    }
+}
