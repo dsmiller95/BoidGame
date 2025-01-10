db9e302864d8029dd4104b0ccda998aab4288915
Dan M <dsmiller95@gmail.com>
2025-01-10 16:36:15 -0600

    add secret code in level 4

---
+++ Assets/Scripts/Other/RainbowText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Other
+{
+    [ExecuteAlways]
+    public class RainbowText : MonoBehaviour
+    {
+        public TMPro.TMP_Text text;
+        public float speed = 1;
+        public float saturation = 1;
+        public float value = 1;
+
+        [SerializeField] private bool inEditMode;
+        
+        private void Update()
+        {
+            if (!Application.isPlaying && !inEditMode) return;
+            
+            var time = Time.time;
+            var hue = time * speed;
+            var color = Color.HSVToRGB(time % 1, saturation, value);
+            text.color = color;
+        }
+    }
+}
