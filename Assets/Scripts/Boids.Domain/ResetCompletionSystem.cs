0ee9546053fd1c764b6a86e5cdd557958d3ec8f9
Dan M <dsmiller95@gmail.com>
2025-01-10 17:44:32 -0600

    reset all boids when moving obstacles

---
+++ Assets/Scripts/Boids.Domain/ResetCompletionSystem.cs
@@ -0,0 +1,37 @@
+﻿using Boids.Domain.Obstacles;
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Boids.Domain
+{
+    [RequireMatchingQueriesForUpdate]
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [BurstCompile]
+    public partial struct ResetCompletionSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+        }
+        
+        public void OnUpdate(ref SystemState state)
+        {
+            // detect if any obstacles are being dragged
+            var draggingQuery = SystemAPI.QueryBuilder()
+                .WithAll<Dragging>()
+                .Build();
+            if (draggingQuery.IsEmpty) return;
+            
+            // if they are, clear out all gameplay values
+
+            var boidQuery = SystemAPI.QueryBuilder()
+                .WithAll<Boid>()
+                .Build();
+            
+            var ecbSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSystem.CreateCommandBuffer(state.WorldUnmanaged);
+            
+            ecb.DestroyEntity(boidQuery, EntityQueryCaptureMode.AtPlayback);
+        }
+    }
+}
